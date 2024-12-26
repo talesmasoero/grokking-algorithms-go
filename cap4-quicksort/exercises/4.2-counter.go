3c1a0c9f8af12a40620badcc69b3620e092a6b81
Tales Masoero <talesmasoero@gmail.com>
2024-12-26 00:40:12 -0300

    Counter

--- cap4-quicksort/exercises/4.2-counter.go
+++ cap4-quicksort/exercises/4.2-counter.go
@@ -12,24 +12,16 @@ func printCounter() {
 	arr := []int{5, 4, 3, 2, 1}
 
 	fmt.Println("Array:", arr)
-	fmt.Println("Counter:", count(arr))
+	fmt.Println("Counter:", counter(arr))
 	fmt.Println()
 }
 
-func count(arr []int) int {
-	// Base cases
+func counter(arr []int) int {
+	// Base case
 	if len(arr) == 0 {
 		return 0
 	}
-	if len(arr) == 1 {
-		return 1
-	}
-
-	var tempArr []int
-
-	tempArr = append(tempArr, arr[1:]...)
-	counter := 1
 
 	// Recursive case
-	return counter + count(tempArr)
+	return 1 + counter(arr[1:])
 }
